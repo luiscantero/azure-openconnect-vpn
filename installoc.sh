760e705855f8c5e93dc209fa1e9475ba97832333
Luis Cantero <luiscantero@hotmail.com>
2019-05-19 10:17:10 +0200

    Corrected eth0 parsing

--- installoc.sh
+++ installoc.sh
@@ -21,7 +21,7 @@ set -e # Exit if error.
 echo "1/7. Install OpenConnect VPN server (ocserv) and Let's Encrypt client (certbot) ..."
 sudo apt install ocserv -y
 sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:certbot/certbot -y
+#sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update
 sudo apt install certbot -y
 
@@ -31,7 +31,7 @@ sudo certbot certonly --standalone --preferred-challenges http --agree-tos -n --
 echo "3/7. Auto-renew certificate daily ..."
 # Auto update OS at 7 AM every Monday.
 (crontab -l 2>/dev/null; echo "0 7 * * 1 apt-get update && apt-get upgrade -y && reboot") | crontab -
-# Auto renews cert on the 1st/15th of every month (done if there are < 30 days remaining).
+# Auto renew cert on the 1st/15th of every month (done if there are < 30 days remaining).
 (crontab -l 2>/dev/null; echo "0 6 1,15 * * certbot renew --quiet --post-hook \"systemctl restart ocserv\"") | crontab -
 # Remove default daily renewal cron job.
 sudo rm /etc/cron.d/certbot
@@ -63,7 +63,7 @@ sudo sed -i '/^#net.ipv4.ip_forward=1/s/^#//' /etc/sysctl.conf
 sudo sysctl -p
 
 echo "6/7. Configure IP masquerading ..."
-eth0="$(ip addr | grep BROADCAST | cut -d ':' -f 2 | tr -d '[:space:]')"
+eth0="$(ip addr | grep BROADCAST | grep -v NO-CARRIER | cut -d ':' -f 2 | tr -d '[:space:]')"
 sudo iptables -t nat -A POSTROUTING -o $eth0 -j MASQUERADE
 
 echo "7/7. Persist IPTable configuration ..."
